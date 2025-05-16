0b895fcda264a77972833c9f0234f3d32edb9ee8
Avijeet Mahal <mahalavijeet5@gmail.com>
2025-05-16 19:23:00 +0530

    third commit

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -7,7 +7,7 @@ const Project = () => {
         <h1>PROJECTS</h1>
         <div className="row d-flex justify-content-center align-items-center">
           {project.map((data) => (<>
-            <div key={data.id} className='my-3 col-sm-6 col-md-4 col-lg-3 mx-3'>
+            <div key={data.id}  className="col-md-4 d-flex justify-content-center align-items-center my-3">
               <div className="card bg-dark text-light" style={{ width: "18rem", border: "1px solid #fff", borderRadius: "10px",boxShadow:"5px 5px 10px 5px rgba(101, 175, 10, 0.5)" }}
                     data-aos="zoom-in" data-aos-duration="1000"
               >
